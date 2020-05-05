40bfcc190860b1beb175d89cf22473f5b0336254
mateusz jakubczak <matispd13@gmail.com>
2020-05-05 19:17:06 +0200

    added manipage and some views stuf

--- korona/database/views.py
+++ korona/database/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+
+from .models import *
+
+
+def index(request):
+    data = Country.objects.Last()
+
+    stu = {
+        "Country": data
+    }
+    return render_to_response("database/mainpage.html", stu)
