7191056320cc45d6d8da0f87bbcb917f80dba94a
mateusz jakubczak <matispd13@gmail.com>
2020-05-19 05:08:17 +0200

    minor changes

--- korona/database/models.py
+++ korona/database/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django_pandas.managers import DataFrameManager
 
 class Country(models.Model):
     Country = models.CharField(max_length=150)
@@ -7,3 +8,5 @@ class Country(models.Model):
     Infected = models.IntegerField()
     Recovered = models.IntegerField()
     Dead = models.IntegerField()
+
+    objects = DataFrameManager()
