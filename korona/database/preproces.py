b9cae730af4f5ab22780334dfa0576f2354304cb
Robert Zydek <rzydek@outlook.com>
2020-05-30 17:26:12 +0200

    country select implementaion

--- korona/database/preproces.py
+++ korona/database/preproces.py
@@ -35,22 +35,26 @@ def Update_Day(Country_selected="Poland"):
             # FIPS, Admin2, Province_State, Country_Region, Last_Update, Lat, Long_, Confirmed, Deaths, Recovered, Active, Combined_Key
             if 'Country/Region' in Country_Data:
                 Country_Data = Country_Data.rename(columns={'Country/Region': 'Country_Region'})
-            Country_Data = Country_Data.loc[Country_Data['Country_Region'] == Country_selected]
-            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-                print(Country_Data)
-            Country_in = Country_Data.loc[Country_Data["Country_Region"] == Country_selected]["Country_Region"].values[0]
-            Date_in = single_date.strftime("%Y-%m-%d")
-            Dead_in = Country_Data.loc[Country_Data["Country_Region"] == Country_selected]["Deaths"].values[0]
-            Infected_in = Country_Data.loc[Country_Data["Country_Region"] == Country_selected]["Confirmed"].values[0]
-            Recoverd_in = Country_Data.loc[Country_Data["Country_Region"] == Country_selected]["Recovered"].values[0]
-            # populate whit data
-            try:
-                country = models.Country(Country=Country_in, Date=Date_in, Dead=Dead_in, Infected=Infected_in, Recovered=Recoverd_in)
-                country.save()
-            except:
-                print("there was a problem with date or country in data popultaion", single_date.strftime("%d-%m-%Y"))
+            for country in Country_Data['Country_Region'].unique():
+                saveToDB(country, single_date, Country_Data)
         except:
             print("No such date")
+
+def saveToDB(country, single_date, Country_Data):
+    Country_in = country
+    Date_in = single_date.strftime("%Y-%m-%d")
+    data = Country_Data.loc[Country_Data['Country_Region'] == country]
+    Dead_in = data.groupby(["Country_Region"]).sum()['Deaths'].values[0]
+    Infected_in = data.groupby(["Country_Region"]).sum()['Confirmed'].values[0]
+    Recoverd_in = data.groupby(["Country_Region"]).sum()['Recovered'].values[0]
+
+    # populate whit data
+    try:
+        country = models.Country(Country=Country_in, Date=Date_in, Dead=Dead_in, Infected=Infected_in, Recovered=Recoverd_in)
+        country.save()
+    except:
+        print("there was a problem with date or country in data popultaion", single_date.strftime("%d-%m-%Y"))
+
 
 def Last_Update_Date():
     return models.Country.objects.last().Date
--- korona/database/views.py
+++ korona/database/views.py
@@ -12,36 +12,43 @@ import datetime
 import numpy as np
 import pandas as pd
 
+selected_country = "Poland"
+
 
 def mainpage(request):
     Update_Day()
     return render(request, "mainpage/mainpage.html")
 
 
-def wykres(request,country = "Poland"):
-    Update_Day()
+def wykres(request):
+    global selected_country
+    if request.GET and request.GET['country']:
+        selected_country = request.GET['country']
+    Update_Day(selected_country)
     df = Get_Full_Data_Frame()
-    # filtter to selected country
-    df = df.loc[df['Country'] == country]
-    fig = go.Figure()
-    Dead = go.Scatter(x=df["Date"], y=df["Dead"], name='Deaths')
-    Inf = go.Scatter(x=df["Date"], y=df["Infected"], name='Confirmed')
-    Rec = go.Scatter(x=df["Date"], y=df["Recovered"], name='Recovered')
-    fig.add_traces([Dead, Inf, Rec])
-    #plotly.offline.plot(fig, filename="database/static_plots/wykers1" + '.html')
-    graph = fig.to_html()
-    context = {'graph': graph, 'Countries': Country.objects.all()}
+    country_list = df['Country'].unique()
+    df = df.loc[df['Country'] == selected_country]
+    context = {
+        'graph': getGraph(df),
+        'Countries': getCountries(df),
+        'country_list': sorted(country_list),
+        'selectedCountry': selected_country
+    }
     return render(request, 'graph/graph.html', context)
 
 
 def trend(request):
-    country = "Poland"
+    global selected_country
+    if request.GET and request.GET['country']:
+        selected_country = request.GET['country']
+    print(selected_country)
     df = Get_Full_Data_Frame()
-    df = df.loc[df['Country'] == country]
+    country_list = df['Country'].unique()
+    df = df.loc[df['Country'] == selected_country]
 
-    fig = make_subplots(rows=3, cols=1, subplot_titles=("Deaths", "Confirmed", "Recovered"))
+    fig = make_subplots(rows=3, cols=1, subplot_titles=(
+        "Deaths", "Confirmed", "Recovered"))
     date = df["Date"]
-
 
     UpperBound = 14
     predictionrange = np.arange(5, UpperBound+date.index[-1])
@@ -50,34 +57,63 @@ def trend(request):
     upper = last + datetime.timedelta(days=UpperBound)
     daterange = pd.date_range(last, upper)
 
-    #prediction for dead
+    # prediction for dead
     yD = df["Dead"]
     poly = np.polyfit(yD.index[10:], yD[10:], 1)
     predictionsDead = np.polyval(poly, predictionrange)
     Dead = go.Scatter(x=date, y=df["Dead"], name='Deaths')
-    DeadPred = go.Scatter(x=daterange , y=predictionsDead[date.index[-14]:], name='Predictions')
+    DeadPred = go.Scatter(
+        x=daterange, y=predictionsDead[date.index[-14]:], name='Predictions')
     fig.append_trace(Dead, row=1, col=1)
     fig.append_trace(DeadPred, row=1, col=1)
 
-    #prediction for infected
+    # prediction for infected
     yI = df["Infected"]
     poly = np.polyfit(yI.index[10:], yI[10:], 1)
     predictionsInf = np.polyval(poly, predictionrange)
     Inf = go.Scatter(x=date, y=df["Infected"], name='Confirmed')
-    InfPred = go.Scatter(x=daterange, y=predictionsInf[date.index[-14]:], name='Prediction')
+    InfPred = go.Scatter(
+        x=daterange, y=predictionsInf[date.index[-14]:], name='Prediction')
     fig.append_trace(Inf, row=2, col=1)
     fig.append_trace(InfPred, row=2, col=1)
 
-    #prediction for Recoverd
+    # prediction for Recoverd
     yR = df["Recovered"]
     poly = np.polyfit(yR.index[30:], yR[30:], 1)
     predictionsRec = np.polyval(poly, predictionrange)
     Rec = go.Scatter(x=date, y=df["Recovered"], name='Recovered')
-    RecPred = go.Scatter(x=daterange, y=predictionsRec[date.index[-14]:], name='Prediction')
+    RecPred = go.Scatter(
+        x=daterange, y=predictionsRec[date.index[-14]:], name='Prediction')
     fig.append_trace(Rec, row=3, col=1)
     fig.append_trace(RecPred, row=3, col=1)
 
-
     graph = fig.to_html(default_height=1500, default_width=1000)
-    context = {'graph': graph}
+    context = {
+        'graph': graph,
+        'country_list': sorted(country_list),
+        'selectedCountry': selected_country
+    }
     return render(request, 'graph/trend.html', context)
+
+
+def getGraph(df):
+    fig = go.Figure()
+    Dead = go.Scatter(x=df["Date"], y=df["Dead"], name='Deaths')
+    Inf = go.Scatter(x=df["Date"], y=df["Infected"], name='Confirmed')
+    Rec = go.Scatter(x=df["Date"], y=df["Recovered"], name='Recovered')
+    fig.add_traces([Dead, Inf, Rec])
+    graph = fig.to_html()
+    return graph
+
+
+def getCountries(df):
+    countryList = []
+    for index, row in df.iterrows():
+        countryList.append(
+            {
+                'confirmed': row['Infected'],
+                'recovered': row['Recovered'],
+                'dead': row['Dead'],
+                'date': row['Date'],
+            })
+    return countryList
